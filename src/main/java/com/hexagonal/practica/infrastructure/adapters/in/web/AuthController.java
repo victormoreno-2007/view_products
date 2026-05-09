03789b69eedc0e394a8553dc250bb45348e0c95a
Victor Moreno <victormorenosamur2007@gmail.com>
2026-05-09 12:15:07 -0500

    feat: :sparkles: Implementación de encriptación de password y vencimiento de este

--- src/main/java/com/hexagonal/practica/infrastructure/adapters/in/web/AuthController.java
+++ src/main/java/com/hexagonal/practica/infrastructure/adapters/in/web/AuthController.java
@@ -1,5 +1,7 @@
 package com.hexagonal.practica.infrastructure.adapters.in.web;
 
+import java.time.Duration;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,6 +13,9 @@ import com.hexagonal.practica.domain.model.user.User;
 import com.hexagonal.practica.domain.ports.in.ManageUserUseCase;
 import com.hexagonal.practica.infrastructure.security.JwtService;
 
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.Bucket;
+import io.github.bucket4j.Refill;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -21,22 +26,31 @@ public class  AuthController {
     private final ManageUserUseCase manageUserUseCase;
     private final JwtService jwtService;
 
+   private final Bucket bucket = Bucket.builder()
+            .addLimit(Bandwidth.classic(5, Refill.greedy(5, Duration.ofMinutes(1))))
+            .build();
 
     @PostMapping("/login")
-    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest request){
+    public ResponseEntity<?> login(@RequestBody LoginRequest request){
         User user = manageUserUseCase.findByEmail(request.email());
 
-        if (user == null) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        if (bucket.tryConsume(1)) {
+            if (user == null) {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+            }
+
+            if (!user.getPassword().equals(request.password())) {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+            }
+
+            String token = jwtService.generateToken(user);
+
+            return ResponseEntity.ok(new AuthResponse(token));
+        } else{
+            return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).body("Has excedido el limite de intentos.");
         }
 
-        if (!user.getPassword().equals(request.password())) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-        }
-
-        String token = jwtService.generateToken(user);
-
-        return ResponseEntity.ok(new AuthResponse(token));
+        
     }
 
 
